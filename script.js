9fdb49e067500fceb3cb1568e964d938e635eea3
Sławek <60218799+Sfafg@users.noreply.github.com>
2025-10-07 09:52:56 +0000

    C_1_2.1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 (function() {
   var ex1_button = document.getElementById('ex1_button');
   var ex1_content = document.getElementById('ex1_content');
@@ -13,7 +14,6 @@
   var ex2_text = document.getElementById('ex2_text');
   var ex2_content = document.getElementById('ex2_content');
 
-
   ex2_text.oninput = function() {
     ex2_content.innerHTML = ""
     if (ex2_text.value.length != 9) ex2_content.innerHTML += "Długość numeru musi być równa 9<br>";
@@ -22,4 +22,17 @@
     if (ex2_content.innerHTML.length == 0) ex2_content.innerHTML += "Numer telefonu jest poprawny<br>";
   }
 
+
+  var ex3_element = document.getElementById('ex3_element');
+  var ex3_one = document.getElementById('ex3_one');
+  var ex3_two = document.getElementById('ex3_two');
+  ex3_element.draggable = "true";
+
+  ex3_two.ondragover = function(e) {
+    e.preventDefault();
+  };
+
+  ex3_two.ondrop = function() {
+    ex3_two.appendChild(ex3_element);
+  }
 })();
