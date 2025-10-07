499675994d6997e583d5e640450ad4be035c78b7
Sławek <60218799+Sfafg@users.noreply.github.com>
2025-10-07 09:21:58 +0000

    C_1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-(function () {
-  //TODO
+(function() {
+  var ex1_button = document.getElementById('ex1_button');
+  var ex1_content = document.getElementById('ex1_content');
+
+  ex1_button.onclick = function() {
+    ex1_content.innerHTML = "";
+    for (var i = 0; i < 10; i++) {
+      ex1_content.innerHTML += i;
+      if (i != 9) ex1_content.innerHTML += ",";
+    }
+
+
+  }
 })();
